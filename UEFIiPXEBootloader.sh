8df9b9c84e0c9cb06a7091f04ebc64e438a37a98
Arda Murteza <134439175+WhiteNeone00@users.noreply.github.com>
2024-10-20 16:27:45 +0200

    Update UEFIiPXEBootloader.sh

--- UEFIiPXEBootloader.sh
+++ UEFIiPXEBootloader.sh
@@ -2,7 +2,7 @@ clear
 echo "Downloading UEFI iPXE Bootloader"
 cd /boot/efi/EFI/ubuntu
 mv grubaa64.efi a.efi
-wget -O grubaa64.efi https://boot.netboot.xyz/ipxe/netboot.xyz-arm64.efi
+wget -O grubaa64.efi https://boot.netboot.xyz/ipxe/netboot.xyz.efi
 cd
 umount /boot/efi
 mount /dev/$(lsblk -lf | grep sda | grep vfat | awk '{print $1}') /boot/efi
